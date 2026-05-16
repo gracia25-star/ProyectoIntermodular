2efca9532ed0c3dab7b5c1729b0061e2295b1758
Lauryspv <perez@velau25@zaragoza.salesianos.edu>
2026-05-16 17:54:09 +0200

    YA TERMINADOOOOOOgit add .git add .

--- src/main/java/com/edugestion/util/ConexionBD.java
+++ src/main/java/com/edugestion/util/ConexionBD.java
@@ -1,14 +1,32 @@
 package com.edugestion.util;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConexionBD {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/departments";
-    private static final String USUARIO = "root";
-    private static final String CONTRASENA = "123456";
+    private static final String URL;
+    private static final String USUARIO;
+    private static final String CONTRASENA;
+
+    static {
+        Properties props = new Properties();
+        try (InputStream is = ConexionBD.class.getClassLoader()
+                .getResourceAsStream("db.properties")) {
+            if (is == null) throw new RuntimeException(
+                "No se encontró db.properties en el classpath");
+            props.load(is);
+        } catch (IOException e) {
+            throw new RuntimeException("Error leyendo db.properties", e);
+        }
+        URL       = props.getProperty("db.url");
+        USUARIO   = props.getProperty("db.usuario");
+        CONTRASENA = props.getProperty("db.contrasena");
+    }
 
     public static Connection getConnection() throws SQLException {
         try {
